edf8621b85a5b464b9cf1151f8e36212eec4bedb
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-12-16 18:46:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 9001 || process.env.PORT;
 
 const categories = require("./public/categories");
 
